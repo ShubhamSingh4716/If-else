e6cfa12b1c8e13de6139deee115e1a37e9eaa238
SHUBHAM SINGH <106438656+ShubhamSingh4716@users.noreply.github.com>
2023-05-09 13:01:45 +0530

    Add files via upload

---
+++ ifElse.js
@@ -0,0 +1,20 @@
+// let age = 12
+// if (age>18)
+// {
+//     console.log("User can play ddlc")
+// }
+// else
+// {
+//     console.log("user cant play")
+// }
+
+// ODD EVEN
+// let num = 56
+// if(num%2==0)
+// {
+//     console.log("number is even")
+// }
+// else
+// {
+//     console.log("number is odd")
+// }
